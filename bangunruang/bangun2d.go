4a3fbeeebf710376970b42aa42c6e314f7980a23
Hendri Karisma <situkangsayur@gmail.com>
2025-11-22 10:57:57 +0700

    add bangunruang untuk bangun2d function

---
+++ bangunruang/bangun2d.go
@@ -0,0 +1,5 @@
+package bangunruang
+
+func HitungLuasPersegiPanjang(panjang int, lebar int) int {
+	return panjang * lebar
+}
--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"tazkia/matdisk/bangunruang"
+)
 
 func main() {
 	fmt.Println("hello world!")
+
+	x := 10
+	y := 11
+
+	var result int = bangunruang.HitungLuasPersegiPanjang(x, y)
+
+	fmt.Println("luas persegi panjang adalah : ", strconv.Itoa(result))
 }
